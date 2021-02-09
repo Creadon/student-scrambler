c5089414516fea05c8d97b27d9fa40fe1c22d4e9
linus <scubegames@gmail.com>
2021-02-09 13:29:50 +0100

    Created project

---
+++ student-scrambler/student-scrambler/Group.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student_scrambler
+{
+    class Group
+    {
+
+        public int Id
+        {
+            get; private set;
+        }
+
+        public List<Student> Students
+        {
+            get; private set;
+        }
+
+        public Group(int Id, List<Student> Students)
+        {
+            this.Id = Id;
+            this.Students = Students;
+        }
+
+    }
+}
---
+++ student-scrambler/student-scrambler/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student_scrambler
+{
+    class Student
+    {
+
+        public int Id
+        {
+            get; private set;
+        }
+
+        public string Name
+        {
+            get; set;
+        }
+
+        public Student(int Id, string Name)
+        {
+            this.Id = Id;
+            this.Name = Name;
+        }
+
+    }
+}
